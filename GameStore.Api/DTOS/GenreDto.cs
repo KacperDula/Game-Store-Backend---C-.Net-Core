f598b272cfc46e01819f915ba52531a82fdbfacd
KacperDula <kacperdulabusiness@gmail.com>
2025-09-01 11:50:36 +0300

    BACKEND

---
+++ GameStore.Api/DTOS/GenreDto.cs
@@ -0,0 +1,4 @@
+namespace GameStore.Api.DTOS;
+
+public record class GenreDto(int Id, string Name);
+
---
+++ GameStore.Api/Entities/Genre.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+
+namespace GameStore.Api.Entities;
+
+public class Genre
+{
+
+    public int Id { get; set; }
+
+
+
+
+    public required string Name { get; set; }
+
+}
